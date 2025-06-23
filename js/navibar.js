efbec024572c90f9187d28e46a9934cceac560e0
Yoshida <sunnri257@gmail.com>
2025-06-23 20:15:25 +0900

    訂正

--- js/navibar.js
+++ js/navibar.js
@@ -6,12 +6,12 @@ $(function(){
   // スクロールした際の処理を指定する
 	$(window).scroll(function () {
     // ナビバー上部がページトップに来た場合
-			if($(window).scrollTop() > navOffset.top) {
-    // ナビバーにクラスを追加する
-		nav.addClass('fixed'); 
-    // それ以外の場合はクラスを外す
+		if($(window).scrollTop() > navOffset.top) {
+    	// ナビバーにクラスを追加する
+			nav.addClass('fixed'); 
+    	// それ以外の場合はクラスを外す
 		}else {
-		nav.removeClass('fixed');
+			nav.removeClass('fixed');
 		}
 	}); 
 });
